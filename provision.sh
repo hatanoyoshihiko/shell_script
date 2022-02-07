d37515ed924ee538d63f70f0d079d46f1c1a4fd1
hatanoyoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2022-02-07 15:40:10 +0900

    add coment

--- provision.sh
+++ provision.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 
+# env
 packages="vim bash-completion chrony vim language-pack-ja-base language-pack-ja"
 os_dist=`cat /etc/os-release`
 
+# .bashrc
 echo "alias nocomment=\"grep -v '^\s*\(#\|$\)'\"" >> ~/.bashrc
 echo "alias vi=vim" >> ~/.bashrc
 source ~/.bashrc
 
+# service control and packages install
 if [[ "$os_dist" =~ "CentOS" ]] || [[ "$os_dist" =~ "AlmaLinux" ]] ; then
 	dnf install -y $packages
 	systemctl stop firewalld.service
@@ -21,13 +24,14 @@ else
 	:
 fi
 
+# set locale and time sync
 localectl set-locale LANG=ja_JP.utf8
 timedatectl set-local-rtc 0
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 locale
-
 chronyc online && chronyc sources
 
+# os update
 if [[ "$os_dist" =~ "CentOS" ]] || [[ "$os_dist" =~ "AlmaLinux" ]] ; then
 	dnf -y upgrade && reboot
 elif [[ "$os_dist" =~ "Ubuntu" ]] ; then

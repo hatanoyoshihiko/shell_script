5f5cdac88821cf9fc07b62da2ee678ed991baa22
Hatano Yoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2025-07-03 20:47:00 +0900

    delete zabbix_defrag.sh, instead of adding mysql_optimize_table.sh

---
+++ mysql_optimize_table.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# MySQL接続情報
+DB_HOST="localhost"
+DB_USER="root"
+DB_PASS="root"
+DB_NAME="zabbix"
+TAG="zabbix-maintenance"
+
+log_info() {
+  logger -t "$TAG" "$1"
+}
+
+log_info "Started maintenance on database: $DB_NAME"
+
+# 全テーブル名の取得
+tables=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -Bse "SHOW TABLES;")
+
+# 各テーブルに対してOPTIMIZE TABLEを実行
+for table in $tables; do
+  echo "Optimizing table: $table"
+  mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "OPTIMIZE TABLE \`$table\`;"
+done
+
+log_info "Maintenance completed on database: $DB_NAME"
--- zabbix_defrag.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-DB_USER=zabbix
-DB_PASSWORD=zabbix
-
-mysql -u $DB_USER -p$DB_PASSWORD -N -e 'USE zabbix; SHOW TABLES;' | xargs -I {} mysql -u $DB_USER -p$DB_PASSWORD -e 'use zabbix; ALTER TABLE {} ENGINE=INNODB;'
-
-
